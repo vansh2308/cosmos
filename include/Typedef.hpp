a91a0d5d23ebd7f645dfbcf85c60a382f4ed76b8
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-11-27 15:30:09 +0530

    Make visitor utils, resolve typedef

---
+++ include/Typedef.hpp
@@ -0,0 +1,15 @@
+#ifndef TYPEDEF_HPP
+#define TYPEDEF_HPP
+
+#include <any>
+#include <memory>
+
+struct Expr;
+struct Stmt;
+
+using unique_expr_ptr = std::unique_ptr<Expr>;
+using unique_stmt_ptr = std::unique_ptr<Stmt>;
+using shared_ptr_any = std::shared_ptr<std::any>;
+
+
+#endif
---
+++ include/Visitor.hpp
@@ -0,0 +1,84 @@
+
+#ifndef VISITOR_HPP
+#define VISTIOR_HPP
+
+#include <any>
+
+struct AssignExpr;
+struct BinaryExpr;
+struct CallExpr;
+struct GetExpr;
+struct GroupingExpr;
+struct LiteralExpr;
+struct LogicalExpr;
+struct SetExpr;
+struct SuperExpr;
+struct ThisExpr;
+struct UnaryExpr;
+struct VarExpr;
+struct ListExpr;
+struct SubscriptExpr;
+struct IncrementExpr;
+struct DecrementExpr;
+
+template <typename T>
+struct ExprVisitor{
+    virtual T visit(const AssignExpr& expr) = 0;
+    virtual T visit(const BinaryExpr& expr) = 0;
+    virtual T visit(const CallExpr& expr) = 0;
+    virtual T visit(const GetExpr& expr) = 0;
+    virtual T visit(const GroupingExpr& expr) = 0;
+    virtual T visit(const LiteralExpr& expr) = 0;
+    virtual T visit(const LogicalExpr& expr) = 0;
+    virtual T visit(const SetExpr& expr) = 0;
+    virtual T visit(const SuperExpr& expr) = 0;
+    virtual T visit(const ThisExpr& expr) = 0;
+    virtual T visit(const UnaryExpr& expr) = 0;
+    virtual T visit(const VarExpr& expr) = 0;
+    virtual T visit(const ListExpr& expr) = 0;
+    virtual T visit(const SubscriptExpr& expr) = 0;
+    virtual T visit(const IncrementExpr& expr) = 0;
+    virtual T visit(const DecrementExpr& expr) = 0;
+    virtual ~ExprVisitor() = default;
+};
+
+struct Expr{
+    virtual ~Expr() = default;
+    virtual std::any accept(ExprVisitor<std::any>& visitor) const = 0;
+};
+
+struct BlockStmt;
+struct ClassStmt;
+struct ExprStmt;
+struct FnStmt;
+struct IfStmt;
+struct PrintStmt;
+struct ReturnStmt;
+struct BreakStmt;
+struct ContinueStmt;
+struct VarStmt;
+struct WhileStmt;
+struct ForStmt;
+
+struct StmtVisitor{
+    virtual void visit(const BlockStmt& stmt) = 0;
+    virtual void visit(const ClassStmt& stmt) = 0;
+    virtual void visit(const ExprStmt& stmt) = 0;
+    virtual void visit(const FnStmt& stmt) = 0;
+    virtual void visit(const IfStmt& stmt) = 0;
+    virtual void visit(const PrintStmt& stmt) = 0;
+    virtual void visit(const ReturnStmt& stmt) = 0;
+    virtual void visit(const BreakStmt& stmt) = 0;
+    virtual void visit(const ContinueStmt& stmt) = 0;
+    virtual void visit(const VarStmt& stmt) = 0;
+    virtual void visit(const WhileStmt& stmt) = 0;
+    virtual void visit(const ForStmt& stmt) = 0;
+    virtual ~StmtVisitor() = default;
+};
+
+struct Stmt{
+    virtual ~Stmt() = default;
+    virtual void accept(StmtVisitor& visitor) const = 0;
+};
+
+#endif
