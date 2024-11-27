78239adf89d189444f31f2aa27dd05bb6a4d0138
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-11-27 15:47:29 +0530

    Declare ExprNode, StmtNode headers

---
+++ include/ExprNode.hpp
@@ -0,0 +1,162 @@
+#ifndef EXPR_HPP
+#define EXPR_HPP
+
+#include "Token.hpp"
+#include "Typedef.hpp"
+#include "Visitor.hpp"
+#include <vector>
+
+struct AssignExpr: Expr{
+    Token identifier;
+    unique_expr_ptr value;
+    AssignExpr(Token identifier, unique_expr_ptr value);
+
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+struct BinaryExpr: Expr{
+    unique_expr_ptr left;
+    Token op;
+    unique_expr_ptr right;
+
+    BinaryExpr(unique_expr_ptr left, Token op, unique_expr_ptr right );
+
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+
+struct UnaryExpr: Expr{
+    Token op;
+    unique_expr_ptr right;
+    UnaryExpr(Token op, unique_expr_ptr right);
+
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+
+struct IncrementExpr: Expr{
+    enum class Type {
+        POSTFIX,
+        PREFIX 
+    };
+
+    Token identifier;
+    Type type;
+
+    IncrementExpr(Token identifier, Type type);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+
+struct DecrementExpr : Expr {
+    enum class Type {
+        POSTFIX,
+        PREFIX
+    };
+
+    Token identifier;
+    Type type;
+
+    DecrementExpr(Token identifier, Type type);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+
+struct CallExpr : Expr{
+    unique_expr_ptr callee;
+    Token paren;
+    std::vector<unique_expr_ptr> args;
+
+    CallExpr(unique_expr_ptr callee, Token paren, std::vector<unique_expr_ptr> args);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+
+
+struct GetExpr : Expr {
+    unique_expr_ptr object;
+    Token identifier;
+
+    GetExpr(unique_expr_ptr object, Token identifier);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+struct SetExpr : Expr {
+    unique_expr_ptr object;
+    Token identifier;
+    unique_expr_ptr value;
+
+    SetExpr(unique_expr_ptr object, Token identifier, unique_expr_ptr value);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+
+struct GroupingExpr: Expr {
+    unique_expr_ptr expression;
+
+    explicit GroupingExpr(unique_expr_ptr expression);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+struct LiteralExpr : Expr {
+    std::any literal;
+
+    explicit LiteralExpr(std::any literal);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+struct LogicalExpr : Expr {
+    unique_expr_ptr left;
+    Token op;
+    unique_expr_ptr right;
+
+    LogicalExpr(unique_expr_ptr left, Token op, unique_expr_ptr right);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+
+
+struct SuperExpr: Expr {
+    Token keyword;
+    Token method;
+
+    SuperExpr(Token keyword, Token method);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+struct ThisExpr : Expr {
+    Token keyword;
+
+    explicit ThisExpr(Token keyword);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+struct VarExpr : Expr {
+    Token identifier;
+
+    explicit VarExpr(Token identifier);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+
+struct ListExpr : Expr {
+    Token opening_bracket;
+    std::vector<unique_expr_ptr> items;
+
+    ListExpr(Token opening_bracket, std::vector<unique_expr_ptr> items);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+
+struct SubscriptExpr : Expr {
+    Token identifier;
+    unique_expr_ptr index;
+    unique_expr_ptr value;
+
+    SubscriptExpr(Token identifier, unique_expr_ptr index, unique_expr_ptr value);
+    std::any accept(ExprVisitor<std::any>& visitor) const override;
+};
+
+
+
+#endif
---
+++ include/StmtNode.hpp
@@ -0,0 +1,125 @@
+
+#ifndef STMT_HPP
+#define STMT_HPP
+
+#include "ExprNode.hpp"
+#include "Token.hpp"
+#include "Typedef.hpp"
+#include "Visitor.hpp"
+#include <vector>
+
+struct BlockStmt: Stmt {
+    std::vector<unique_stmt_ptr> statements;
+
+    explicit BlockStmt(std::vector<unique_stmt_ptr> statements);
+    void accept(StmtVisitor& visitor) const override;
+};
+
+
+struct ClassStmt : Stmt {
+    Token identifier;
+    std::unique_ptr<VarExpr> superclass; // OPTIONAL
+    std::vector<std::unique_ptr<FnStmt>> methods;
+
+    ClassStmt(Token identifier, std::vector<std::unique_ptr<FnStmt>> methods, std::unique_ptr<VarExpr> superclass);
+    void accept(StmtVisitor& visitor) const override;
+};
+
+struct ExprStmt : Stmt {
+    unique_expr_ptr expression;
+
+    explicit ExprStmt(unique_expr_ptr expr);
+    void accept(StmtVisitor& visitor) const override;
+};
+
+
+
+struct FnStmt : Stmt {
+    Token identifier;
+    std::vector<Token> params;
+    std::vector<unique_stmt_ptr> body;
+
+    FnStmt(Token identifier, std::vector<Token> params, std::vector<unique_stmt_ptr> body);
+    void accept(StmtVisitor& visitor) const override;
+};
+
+
+struct IfBranch {
+    unique_expr_ptr condition;
+    unique_stmt_ptr statement;
+
+    IfBranch(unique_expr_ptr condition, unique_stmt_ptr statement);
+};
+
+struct IfStmt : Stmt {
+    IfBranch main_branch;
+    std::vector<IfBranch> elif_branches;
+    unique_stmt_ptr else_branch; // OPTIONAL
+
+    IfStmt(IfBranch main_branch, std::vector<IfBranch> elif_branches, unique_stmt_ptr else_branch);
+    void accept(StmtVisitor& visitor) const override;
+};
+
+struct PrintStmt : Stmt {
+    unique_expr_ptr expression; // OPTIONAL
+
+    explicit PrintStmt(unique_expr_ptr expr);
+    void accept(StmtVisitor& visitor) const override;
+};
+
+
+
+struct ReturnStmt : Stmt {
+    Token keyword;
+    unique_expr_ptr expression; // OPTIONAL
+
+    ReturnStmt(Token keyword, unique_expr_ptr expr);
+    void accept(StmtVisitor& visitor) const override;
+};
+
+struct ContinueStmt : Stmt {
+    Token keyword;
+
+    explicit ContinueStmt(Token keyword);
+    void accept(StmtVisitor& visitor) const override;
+};
+
+struct BreakStmt : Stmt {
+    Token keyword;
+
+    explicit BreakStmt(Token keyword);
+    void accept(StmtVisitor& visitor) const override;
+};
+
+struct VarStmt : Stmt {
+    Token identifier;
+    unique_expr_ptr initializer; // OPTIONAL
+
+    VarStmt(Token identifier, unique_expr_ptr initializer);
+    void accept(StmtVisitor& visitor) const override;
+};
+
+
+struct WhileStmt : Stmt {
+    unique_expr_ptr condition;
+    unique_stmt_ptr body;
+
+    WhileStmt(unique_expr_ptr condition, unique_stmt_ptr body);
+    void accept(StmtVisitor& visitor) const override;
+};
+
+struct ForStmt : Stmt {
+    unique_stmt_ptr initializer;
+    unique_expr_ptr condition;
+    unique_expr_ptr increment;
+    unique_stmt_ptr body;
+
+    ForStmt(unique_stmt_ptr initializer, unique_expr_ptr condition, unique_expr_ptr increment, unique_stmt_ptr body);
+    void accept(StmtVisitor& visitor) const override;
+};
+
+
+
+
+
+#endif
